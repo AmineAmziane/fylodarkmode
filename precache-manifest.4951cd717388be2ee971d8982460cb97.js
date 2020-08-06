8a19fc50d5bc2c9621056467d2cecb7cdf1f76a0
AmineDz <amine2014max@gmail.com>
2020-08-06 13:10:51 +0100

    Updates

---
+++ precache-manifest.4951cd717388be2ee971d8982460cb97.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f9c0c4303b83aadf460d77a9932d1a8",
+    "url": "/fylodarkmode/index.html"
+  },
+  {
+    "revision": "d554e3acca84ea35c5e7",
+    "url": "/fylodarkmode/static/css/main.f9d73fb5.chunk.css"
+  },
+  {
+    "revision": "7441257fcca77d69af34",
+    "url": "/fylodarkmode/static/js/2.a3c0d84c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fylodarkmode/static/js/2.a3c0d84c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d554e3acca84ea35c5e7",
+    "url": "/fylodarkmode/static/js/main.c1874914.chunk.js"
+  },
+  {
+    "revision": "8b96d442f7e619076e5b",
+    "url": "/fylodarkmode/static/js/runtime-main.dfd9d521.js"
+  },
+  {
+    "revision": "376a8b95194fb33e0a203bf52f8cb467",
+    "url": "/fylodarkmode/static/media/bg-curvy-desktop.376a8b95.svg"
+  },
+  {
+    "revision": "bb63956fe01de6ad8e7622bf44d33529",
+    "url": "/fylodarkmode/static/media/bg-curvy-mobile.bb63956f.svg"
+  },
+  {
+    "revision": "5f4b8b3e0d56ac7578b1468584142faf",
+    "url": "/fylodarkmode/static/media/icon-access-anywhere.5f4b8b3e.svg"
+  },
+  {
+    "revision": "aedb3ae079501c43082669d8d427bf6b",
+    "url": "/fylodarkmode/static/media/icon-any-file.aedb3ae0.svg"
+  },
+  {
+    "revision": "794c5a484aefc6409c3a4e7aceb10eee",
+    "url": "/fylodarkmode/static/media/icon-arrow.794c5a48.svg"
+  },
+  {
+    "revision": "ca8c71cfbd876e461e850fcdcf9c9b29",
+    "url": "/fylodarkmode/static/media/icon-collaboration.ca8c71cf.svg"
+  },
+  {
+    "revision": "c90d96c2492a2d6f0863256f2bf9015d",
+    "url": "/fylodarkmode/static/media/icon-email.c90d96c2.svg"
+  },
+  {
+    "revision": "0f65814bd9f282828254e3382e223a57",
+    "url": "/fylodarkmode/static/media/icon-location.0f65814b.svg"
+  },
+  {
+    "revision": "47e54a44b879380ff8adb31f40249296",
+    "url": "/fylodarkmode/static/media/icon-phone.47e54a44.svg"
+  },
+  {
+    "revision": "f558537850876022cbf1d99e525724f4",
+    "url": "/fylodarkmode/static/media/icon-security.f5585378.svg"
+  },
+  {
+    "revision": "526544ae64c3d7a9cf06bc566728d2b9",
+    "url": "/fylodarkmode/static/media/illustration-intro.526544ae.png"
+  },
+  {
+    "revision": "99294b1f2757d664921ec643a982959a",
+    "url": "/fylodarkmode/static/media/illustration-stay-productive.99294b1f.png"
+  },
+  {
+    "revision": "c71bf7bf6985f3ac03fa893fb45bed38",
+    "url": "/fylodarkmode/static/media/logo.c71bf7bf.svg"
+  }
+]);
